f667905c0cc6b36c408b6b0592b8083cdc39cb5a
markser <serm@oregonstate.edu>
2021-05-01 11:42:02 -0700

    added non_complex_number case to volume

--- testVolumeOfCube.py
+++ testVolumeOfCube.py
@@ -8,6 +8,17 @@ import volumeOfCube
 # had help with https://stackoverflow.com/questions/18161330/using-unittest-mock-to-patch-input-in-python-3/37467870 and 
 
 class TestCalculator(unittest.TestCase):
+    @patch('builtins.input', return_value="6")
+    def test_non_complex_numbers(self,mock_input):
+        # with unittest.mock.patch('volumeOfCube.retrieve_input', return_value=-1):
+        capturedOutput = io.StringIO()                  # Create StringIO object
+        sys.stdout = capturedOutput                     #  and redirect stdout.
+        volumeOfCube.main()                                     # Call function.
+        sys.stdout = sys.__stdout__                     # Reset redirect.
+        expected = "Volume of Cube: 18.0\n"
+        actual = capturedOutput.getvalue()
+        self.assertEqual(actual, expected)
+
     @patch('builtins.input', return_value="1.02")
     def test_complex_numbers(self,mock_input):
         # with unittest.mock.patch('volumeOfCube.retrieve_input', return_value=-1):
